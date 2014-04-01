49cc2b1afac35d53f092c2cad730e1d685b505fa
Gavin M. Roy <gavinr@aweber.com>
2014-04-01 15:30:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+RabbitMQ Wrapper for epgsql
+===========================
+Clone this into rabbitmq-public-umbrella for use as a dependency by
+plugins that require it.
---
+++ package.mk
@@ -0,0 +1,15 @@
+APP_NAME:=epgsql
+
+UPSTREAM_GIT:=https://github.com/wg/epgsql.git
+UPSTREAM_REVISION:=3318bd5d646cad0623a
+RETAIN_ORIGINAL_VERSION:=true
+
+# Path include/http.hrl is needed during compilation
+INCLUDE_DIRS+=$(CLONE_DIR)
+
+ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/$(APP_NAME).app.src
+DO_NOT_GENERATE_APP_FILE=true
+
+define construct_app_commands
+	cp $(CLONE_DIR)/LICENSE $(APP_DIR)/LICENSE-BSD-epgsql
+endef
